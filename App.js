179413031df1663948527e6d552c81c32fe33c17
SuviAnnina <suvi.varjoranta@gmail.com>
2024-03-05 20:12:21 +0200

    Shoppinglist with SQLite database

--- App.js
+++ App.js
@@ -1,27 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, } from 'react-native';
+import * as SQLite from 'expo-sqlite';
 
 export default function App() {
+
+  const [product, setProduct] = useState("");
+  const [amount, setAmount] = useState("");
+  const [items, setItems] = useState([]);
+
+  const database = SQLite.openDatabase('itemsdb.db');
+
+  useEffect(() => {
+    database.transaction(tx => {
+      tx.executeSql('create table if not exists shoppinglist (id integer primary key not null, product text, amount text);');
+    }, () => console.error("Error when creating DB"), updateList);
+  }, []);
+
+  const saveItem = () => {
+    database.transaction(tx => {
+      tx.executeSql('insert into shoppinglist (product, amount) values (?, ?);',
+        [product, amount]);
+    }, null, updateList)
+    setProduct("");
+    setAmount("");
+  }
+
+  const updateList = () => {
+    database.transaction(tx => {
+      tx.executeSql('select * from shoppinglist;', [], (_, { rows }) =>
+        setItems(rows._array)
+      );
+    }, null, null);
+  }
+
+  const deleteItem = (id) => {
+    database.transaction(tx => {
+      tx.executeSql('delete from shoppinglist where id = ?;', [id]);
+    }, null, updateList)
+  }
+
+
   return (
     <View style={styles.container}>
       <View style={styles.productInput}>
         <TextInput
           style={styles.textInput}
           placeholder="product"
+          onChangeText={product => setProduct(product)}
+          value={product}
         />
         <TextInput
           style={styles.textInput}
           placeholder="amount"
+          onChangeText={amount => setAmount(amount)}
+          value={amount}
         />
 
         <Button
           title="Save"
+          onPress={saveItem}
         />
 
       </View>
 
       <View style={styles.shoppingList} >
         <Text>Shopping list</Text>
+
+        <FlatList
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) =>
+            <View style={styles.listContainer}>
+              <Text>{item.product},{item.amount} </Text>
+              <Text style={{ color: '#0000ff' }} onPress={() => deleteItem(item.id)}>done</Text>
+            </View>}
+          data={items}
+        />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -49,5 +103,12 @@ const styles = StyleSheet.create({
   },
   shoppingList: {
     flex: 4,
+    width: "70%",
+    alignItems: "center"
+  },
+  listContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
   }
 });
