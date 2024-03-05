c1b3c4e83a7878dc226b2fccc2151c66933d2d3f
SuviAnnina <suvi.varjoranta@gmail.com>
2024-03-05 19:36:50 +0200

    first add project base

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.productInput}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="product"
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="amount"
+        />
+
+        <Button
+          title="Save"
+        />
+
+      </View>
+
+      <View style={styles.shoppingList} >
+        <Text>Shopping list</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +35,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  productInput: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center", //2nd: vasen-oikea
+    justifyContent: "flex-end" // primary: ylä-ala
+  },
+  textInput: {
+    borderColor: 'black',
+    borderWidth: 2,
+    width: "40%",
+    marginBottom: "2%",
+  },
+  shoppingList: {
+    flex: 4,
+  }
 });
